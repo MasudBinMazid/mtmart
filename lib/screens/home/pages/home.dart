036f2566ecc1b3bc49b17f40c59b3069fabc0038
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-03-19 09:50:03 +0600

    bottomnavigationbar

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -9,10 +9,35 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-          title: "Mega shop",
-          firsticon: CupertinoIcons.bell,
-          secondicon: CupertinoIcons.cart,
-          showBackButton: true,),
+        title: "Mega shop",
+        firsticon: CupertinoIcons.bell,
+        secondicon: CupertinoIcons.cart,
+        showBackButton: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.home, color: Colors.black), 
+        label: "HOME",
+          ),
+          BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.heart, color: Colors.black), 
+        label: "WISHLIST",
+          ),
+          BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.bag, color: Colors.black), 
+        label: "ORDER",
+          ),
+          BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.person, color: Colors.black), 
+        label: "ACCOUNT",
+          ),
+        ],
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+      ),
       body: Column(),
     );
   }
